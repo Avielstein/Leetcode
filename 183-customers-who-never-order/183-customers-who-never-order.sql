3930d17fcc17ad15b955b1f784d7fd6e894d154d
Aviel Stein <ajs568@drexel.edu>
2022-06-20 22:28:22 -0400

    Time: 390 ms (96.06%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,7 +1,11 @@
 # Write your MySQL query statement below
-select customers.name as 'Customers'
-from customers
-where customers.id not in
-(
-    select customerid from orders
-);
+select 
+    customers.name as 'Customers'
+from 
+    customers
+where 
+    customers.id not in
+        (
+            select customerid from orders
+        )
+        ;
