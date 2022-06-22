c89e7ca877e1db994e9476fbd5ad1d914523a990
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-06-22 15:54:00 -0400

    Time: 921 ms (6.69%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -6,7 +6,12 @@ SELECT
     COUNT(DISTINCT product) num_sold, #
     GROUP_CONCAT(DISTINCT product) products
 FROM activities
-GROUP BY sell_date
-#ORDER BY sell_date
+GROUP BY sell_date 
 
-#The sold products names for each date should be sorted lexicographically.
+#Return the result table ordered by sell_date.
+ORDER BY sell_date #
+#already ordered by sell date, but this will make sure if you have 
+#them in a random order, then it will list cronolocally
+
+#where is this taken care of:
+#"The sold products names for each date should be sorted lexicographically."?
