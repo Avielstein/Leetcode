4939ad1ddcac787e96a9da5b70b9360cdd2d0064
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-06-22 15:51:13 -0400

    Time: 363 ms (94.51%), Space: 0B (100.00%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
+
+
 SELECT
-    sell_date,
-    COUNT(DISTINCT product) num_sold,
+    sell_date, #date the number of different products sold and their names.
+    COUNT(DISTINCT product) num_sold, #
     GROUP_CONCAT(DISTINCT product) products
 FROM activities
 GROUP BY sell_date
-ORDER BY sell_date
+#ORDER BY sell_date
+
+#The sold products names for each date should be sorted lexicographically.
