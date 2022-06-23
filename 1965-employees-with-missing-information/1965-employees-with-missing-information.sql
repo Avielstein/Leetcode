864c0d242b8507e84e77d0ec8c2597192bdec3b9
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-06-23 18:00:21 -0400

    Time: 2147 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,9 +1,18 @@
 # Write your MySQL query statement below
+
+#topic of interst
 SELECT T.employee_id
-FROM  
-  (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
-   UNION 
-   SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
+
+#data of interest
+FROM
+    #Right join + left join
+    (SELECT * FROM Employees LEFT JOIN Salaries USING(employee_id)
+     UNION 
+     SELECT * FROM Employees RIGHT JOIN Salaries USING(employee_id))
 AS T
+
+#A line that has missing information
 WHERE T.salary IS NULL OR T.name IS NULL
+
+#sort
 ORDER BY employee_id;
