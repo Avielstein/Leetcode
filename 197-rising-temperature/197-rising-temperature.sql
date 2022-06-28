79c1c48db5dfe003b4e0ab02b2344baada16816d
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-06-27 21:56:32 -0400

    Time: 714 ms (18.17%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -4,4 +4,5 @@ FROM Weather AS w1 , Weather AS w2
 #here the are using specialized function "datediff", to find consecutive days
 #assumably there is some general diff function that can help us comparing different
 #values
+#https://www.w3schools.com/sql/func_sqlserver_datediff.asp
 WHERE w1.Temperature > w2.Temperature AND DATEDIFF(w1.recordDate , w2.recordDate) = 1
