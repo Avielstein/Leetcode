3b52a97d295ee0ed05252aec77f04f8c4d86ea0e
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-06-28 16:10:08 -0400

    Time: 2272 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -5,7 +5,7 @@ COUNT(DISTINCT lead_id) AS unique_leads,
 COUNT(DISTINCT partner_id) AS unique_partners
 
 FROM DailySales
-GROUP BY 1, 2 #sort by two things at once, very cool 
+GROUP BY 1, 2 #Group by two things at once, very cool 
 
 #made a bit more readable, IMO
 #from: https://leetcode.com/problems/daily-leads-and-partners/discuss/976703/Simple-MySQL-solution-using-COUNT-DISTINCT
