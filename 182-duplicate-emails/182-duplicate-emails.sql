86206485f749be21b5c644ea4764358209b0e4b2
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-06-29 17:08:11 -0400

    Time: 307 ms (90.38%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
+/*
 Select Email
 From Person
 GROUP BY Email
 Having count(Email)>1
+*/
+
+SELECT DISTINCT a.Email
+  FROM Person AS a , Person AS b
+    WHERE a.Id <> b.Id AND a.Email = b.Email
