7da64a7ed73e19af78148fc7840f96a62c011198
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-06-29 18:00:50 -0400

    Time: 37 ms (55.62%), Space: 13.2 MB (91.12%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMiddleIndex(self, n):
+        
+        for i in range(len(n)):
+            #test pivot condition
+            if sum(n[:i])==sum(n[i+1:]): 
+                
+                #If the index is on the left edge of the array, then the 
+                #left sum is 0 because there are no elements to the left. 
+                #This also applies to the right edge of the array.
+                if i==0 or i == len(n):
+                    return 0
+                
+                #the index of the pivot
+                return i
+        
+        
+        #Return the leftmost pivot index. If no such index exists, return -1.
+        return -1
+        
+        
