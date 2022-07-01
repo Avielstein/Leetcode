c1065a4f4b67bf85b591e26eb0c268c946bb59ff
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-07-01 12:55:51 -0400

    Time: 24 ms (70.50%), Space: 13.8 MB (22.16%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -1,19 +1,21 @@
 class Solution(object):
     def isSubsequence(self, s, t):
         
-        #
+        #set up var and check for empty string
         start = 0
         if start==len(s): return True
         
-        
         for i in range(len(t)):  
-            #print(t[i], s[start])
+            
+            #we found the whole substring
             if start >= len(s):
                 return True
             
+            #we found the letter
             if t[i] == s[start]:
                 start+=1
-            
+        
+        #final check
         if start == len(s): 
             return True
         else: 
