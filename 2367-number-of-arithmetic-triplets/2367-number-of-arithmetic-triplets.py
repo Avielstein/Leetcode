929b11c07fd7e1b01be547732d12cbff7def7fc1
Aviel Stein, Ph.D <ajs568@drexel.edu>
2022-08-17 14:20:12 -0400

    Time: 55 ms (51.07%), Space: 13.5 MB (15.75%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -1,8 +1,11 @@
 class Solution(object):
     def arithmeticTriplets(self, n, d):
         count=0
-        for i in range(len(n)):
+        #for each number in the list we have to check if there is an arithmetic tripple
+        for i in range(len(n)-2): #we only need to check up to the last 3 because we know there cant be more
+            #so we see if we add the differences to our number if they exist in the list
             if n[i]+d in n and n[i]+(2*d) in n:
+                #proof
                 #print(n[i],n[i]+d,n[i]+(2*d))
                 count+=1
         return count
